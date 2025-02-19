5f181fe2495e71107319a24eee4fce3d672d8b2e
sheshi vardhan <123223662+nsvoriginals@users.noreply.github.com>
2025-02-19 10:59:16 +0530

    Update authRoute.js

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -12,9 +12,7 @@ router.post("/register", async (req, res) => {
         console({email});
 
         
-        const existingUser = await prisma.user.findUnique({
-            where: { email },
-        });
+       
 
         if (existingUser) {
             return res.status(400).json({ message: 'Email already in use' });
